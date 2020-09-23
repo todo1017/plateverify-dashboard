9d5b10039841bcd7d2313539ac68861ef215816d
todo1017 <todo1017@outlook.com>
2020-09-23 23:21:32 +0800

    remove material-ui-picker

--- src/containers/layouts/schoolLayout.js
+++ src/containers/layouts/schoolLayout.js
@@ -1,8 +1,6 @@
 import React from "react";
-import MomentUtils from "@date-io/moment";
 import { ThemeProvider } from "@material-ui/styles";
 import { createMuiTheme } from "@material-ui/core/styles";
-import { MuiPickersUtilsProvider } from "material-ui-pickers";
 import VerticalLayout from "./verticalLayout";
 import indigoTheme from "./themes/indigoTheme";
 import "assets/vendors/style";
@@ -12,11 +10,9 @@ const applyTheme = createMuiTheme(indigoTheme);
 const SchoolLayout = ({ children }) => {
   return (
     <ThemeProvider theme={applyTheme}>
-      <MuiPickersUtilsProvider utils={MomentUtils}>
-        <VerticalLayout>
-          {children}
-        </VerticalLayout>
-      </MuiPickersUtilsProvider>
+      <VerticalLayout>
+        {children}
+      </VerticalLayout>
     </ThemeProvider>
   );
 };
